aedfba72e1b9269f6beb51688c7bc0935d3d55d1
Amelia Lin <amelia.lin0@gmail.com>
2019-07-18 16:33:12 -0700

    Make output more readable

--- news.py
+++ news.py
@@ -1,4 +1,5 @@
 import psycopg2
+from pprint import pprint
 
 DBNAME = "news"
 
@@ -17,9 +18,24 @@ c.execute('''
     '''
     )
 results = c.fetchall()
-print 'results: \n', results
+print 'Most popular articles:'
+pprint(results)
 
 # 2. Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views?
+
+# c.execute('''
+#     select title, count(*) as num 
+#     from articles join log join authors
+#     on
+#         concat('/article/', articles.slug) = log.path
+#         and articles.author = authors.id
+#     group by title
+#     order by num desc
+#     limit 3;
+#     '''
+#     )
+# results = c.fetchall()
+# print 'Most popular authors: \n', results
 
 # 3. On which days did more than 1% of requests lead to errors?
 
