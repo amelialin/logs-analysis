f0a361df15402587be1d2a67ffce17b434b8541b
Amelia Lin <amelia.lin0@gmail.com>
2019-07-18 16:17:39 -0700

    Calculate 3 most popular articles

--- news.py
+++ news.py
@@ -4,19 +4,23 @@ DBNAME = "news"
 
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
+
+# 1. What are the most popular three articles of all time? Which articles have been accessed the most?
+
 c.execute('''
-    select * 
-    from authors
-    limit 1;
+    select title, count(*) as num 
+    from articles join log
+    on concat('/article/', articles.slug) = log.path
+    group by title
+    order by num desc
+    limit 3;
     '''
     )
 results = c.fetchall()
 print 'results: \n', results
-db.close()
-
-# 1. What are the most popular three articles of all time? Which articles have been accessed the most?
 
 # 2. Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views?
 
 # 3. On which days did more than 1% of requests lead to errors?
 
+db.close()
