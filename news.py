7767679d7e0d88625e59eaf0e4e8abaf4473a38e
Amelia Lin <amelia.lin0@gmail.com>
2019-07-18 22:44:49 -0700

    Add docstrings, move functionality out of module scope, improve logging

--- news.py
+++ news.py
@@ -1,65 +1,58 @@
 #!/usr/bin/env python3
 
 import psycopg2
-from pprint import pprint
 
-DBNAME = "news"
+DATABASE_NAME = "news"
 OUTPUT_FILE = "output.txt"
 
-database_connection = psycopg2.connect(database=DBNAME)
-cursor = database_connection.cursor()
-
-# 1. What are the most popular three articles of all time? Which articles have been accessed the most?
-
 def get_top_articles(cursor):
+    """Returns list of tuples of (article title, views) for top 3 viewed articles, ranked by views descending."""
     cursor.execute('''
-        select title, count(*) as num 
-        from articles join log
-            on concat('/article/', articles.slug) = log.path
-        group by title
-        order by num desc
-        limit 3;
+        SELECT title, count(*) AS num
+        FROM articles JOIN log
+            ON concat('/article/', articles.slug) = log.path
+        GROUP BY title
+        ORDER BY num desc
+        LIMIT 3;
         '''
         )
     return cursor.fetchall()
-
-# 2. Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views?
 
 def get_top_authors(cursor):
+    """Returns list of tuples of (author, views of all authored articles) for all authors, ranked by views descending."""
     cursor.execute('''
-        select authors.name, count(*) as num 
-        from articles join log 
-            on concat('/article/', articles.slug) = log.path
-        join authors
-            on articles.author = authors.id
-        group by authors.name
-        order by num desc;
+        SELECT authors.name, count(*) AS num
+        FROM articles JOIN log
+            ON concat('/article/', articles.slug) = log.path
+        JOIN authors
+            ON articles.author = authors.id
+        GROUP BY authors.name
+        ORDER BY num desc;
         '''
         )
     return cursor.fetchall()
 
-# 3. On which days did more than 1% of requests lead to errors?
-
 def get_top_error_days(cursor):
+    """Returns list of tuples of (date, percent) for all days ON which more than 1% of requests led to errors, ordered by date."""
     cursor.execute('''
-        select * from
-            (select count_total.day, round((errors * 100.00 / total), 1) as percent_error
-            from 
-                (select CAST(time AS date) as day, count(*) as total
-                    from log
-                    group by day
-                    order by day asc
-                ) as count_total 
-                join (select CAST(time AS date) as day, count(*) as errors
-                    from log
-                    where status != '200 OK'
-                    group by day
-                    order by day asc
-                ) as count_errors
-                on count_total.day = count_errors.day
-            ) as a
-            where percent_error > 2
-            order by day asc
+        SELECT * FROM
+            (SELECT count_total.day, round((errors * 100.00 / total), 1) AS percent_error
+            FROM
+                (SELECT CAST(time AS date) AS day, count(*) AS total
+                    FROM log
+                    GROUP BY day
+                    ORDER BY day ASC
+                ) AS count_total
+                JOIN (SELECT CAST(time AS date) AS day, count(*) AS errors
+                    FROM log
+                    WHERE status != '200 OK'
+                    GROUP BY day
+                    ORDER BY day ASC
+                ) AS count_errors
+                ON count_total.day = count_errors.day
+            ) AS a
+            WHERE percent_error > 2
+            ORDER BY day ASC
         ;
         '''
         )
@@ -82,21 +75,28 @@ On which days did more than 1% of requests lead to errors?
 {top_error_days}
 '''.strip()
 
-with open(OUTPUT_FILE, 'w') as fp:
+if __name__ == '__main__':
+    print('Connecting to database...')
+    database_connection = psycopg2.connect(database=DATABASE_NAME)
+    cursor = database_connection.cursor()
+    print('Connection successful...')
+
+    print('Querying for top articles...')
     top_articles = '\n'.join('"{}" - {:,} views'.format(article_title.title(), views) 
-                             for (article_title, views) in get_top_articles(cursor))
+                                for (article_title, views) in get_top_articles(cursor))
+    print('Querying for top authors...')
     top_authors = '\n'.join('{} - {:,} views'.format(author, views)
                             for (author, views) in get_top_authors(cursor))
+    print('Querying for top error days...')
     top_error_days = '\n'.join('{:%B %d}, {:%Y} - {}% errors'.format(day, day, error_percent)
-                               for (day, error_percent) in get_top_error_days(cursor))
+                            for (day, error_percent) in get_top_error_days(cursor))
     
-    fp.write(OUTPUT_TEMPLATE.format(top_articles=top_articles,
-                                    top_authors=top_authors,
-                                    top_error_days=top_error_days))
+    print('Writing to output.txt...')
+    with open(OUTPUT_FILE, 'w') as fp:
+        fp.write(OUTPUT_TEMPLATE.format(top_articles=top_articles,
+                                        top_authors=top_authors,
+                                        top_error_days=top_error_days))
+    print('Writing complete...')
 
-database_connection.close() 
-
-# if __name__ == '__main__':
-#     database_connection = psycopg2.connect(database=DBNAME)
-#     c = db.cursor()
-#     db.close()
+    database_connection.close()
+    print('Connection closed. All done!')
